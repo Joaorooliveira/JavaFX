fd15837af3a68036e81e8ed719a94b202a721e72
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-10-23 21:30:25 -0300

    Treinando com JavaFX

--- src/main/java/br/com/joaov/javafxminhaaplicacao/ExemplosLayouts.java
+++ src/main/java/br/com/joaov/javafxminhaaplicacao/ExemplosLayouts.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.Scene;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -20,16 +19,17 @@ public class ExemplosLayouts extends Application {
         Button botao2 = new Button("Botao 2");
         Button botao3 = new Button("Botao 3");
         Button botao4 = new Button("Botao 4");
+        Button botao5 = new Button("Botao 5");
 
         HBox hBox = new HBox(botao1,botao2);
-        hBox.setAlignment(Pos.CENTER);
+        hBox.setAlignment(Pos.CENTER_RIGHT);
 
         VBox vBox = new VBox(botao3,botao4);
-        vBox.setAlignment(Pos.CENTER);
+        vBox.setAlignment(Pos.CENTER_RIGHT);
 
         BorderPane borderPane = new BorderPane();
         borderPane.setTop(hBox);
-        borderPane.setCenter(vBox);
+        borderPane.setBottom(vBox);
 
         Scene scene = new Scene(borderPane,300,300);
         palco.setScene(scene);
